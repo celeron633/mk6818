02d5508fafbd402e5070fa1358999e49794a4e78
celeron633 <dengxionghui@hotmail.com>
2024-11-30 21:07:58 +0800

    mk6818: add script to generate bootable file

---
+++ gen_gec6818_uboot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+uboot_raw_bin_path="/home/dengxh/data/u-boot_nanopi3/u-boot.bin"
+out_file_name="boot.bin"
+nsih_name="drone-sd-64.txt"
+bl1_name="bl1-drone.bin"
+bootloader_name="u-boot.bin"
+
+cp ../bl1-artik710/out/${bl1_name} ./
+cp ../u-boot_nanopi3/${bootloader_name} ./
+
+./mk6818 ${out_file_name} ${nsih_name} ${bl1_name} ${uboot_raw_bin_path} 1
+echo "done!"
